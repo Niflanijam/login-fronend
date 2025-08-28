ef89e89f44099d442a3dde3872f0336cf902d4ca
Nifla <fathimanifla1118@gmail.com>
2025-08-28 14:04:16 +0530

    home page chANGES

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,4 @@
-/*import React, { useState } from "react";
-import { Box, Typography } from "@mui/material";
-import Header from "../components/Header";
-import DietForm from "../components/DietForm";
-import DietResult from "../components/DietResult";
-import Diet from "../components/Diet";
 
-export default function Home() {
-  const [result, setResult] = useState(null);
-
-  return (
-    <>
-      <Header />
-      <Diet />
-      <Box sx={{ p: 4, background: "#f7f7f7", minHeight: "100vh" }}>
-        <Typography variant="h4" gutterBottom sx={{ fontWeight: "bold" }}>
-          Personalized Diet Planner
-        </Typography>
-        <DietForm onResult={setResult} />
-        
-      </Box>
-    </>
-  );
-}
-*/
 
 import React, { useState } from "react";
 import { Box, Typography } from "@mui/material";
